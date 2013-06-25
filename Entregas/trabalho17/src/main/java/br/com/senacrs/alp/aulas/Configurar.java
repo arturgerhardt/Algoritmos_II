06b06d05bcb8aa0f1e7818610b4ccbcd8d9ac59c
Artur <blackcanary86@gmail.com>
2013-06-25 19:58:23 -0300

    Abrindo

---
+++ Entregas/trabalho17/src/main/java/br/com/senacrs/alp/aulas/Configurar.java
@@ -0,0 +1,27 @@
+package br.com.senacrs.alp.aulas;
+
+public class Configurar implements ArquivoConfiguracao {
+
+	public Configurar(String arquivoConfiguracao) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getRootDir() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getErrorDir() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPort() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Entregas/trabalho17/src/main/java/br/com/senacrs/alp/aulas/Validar.java
@@ -0,0 +1,18 @@
+package br.com.senacrs.alp.aulas;
+
+import java.io.Reader;
+import java.io.Writer;
+
+public class Validar implements ResponderRequisicaoGet {
+
+	public Validar(ArquivoConfiguracao config) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void responderRequisicao(Reader requisicao, Writer saida) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
