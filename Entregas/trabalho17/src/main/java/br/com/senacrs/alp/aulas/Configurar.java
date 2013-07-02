05e71fd32d72f052c7db9a8599edddb8bdf92b89
Artur <blackcanary86@gmail.com>
2013-07-02 19:47:12 -0300

    teste

--- Entregas/trabalho17/src/main/java/br/com/senacrs/alp/aulas/Configurar.java
+++ Entregas/trabalho17/src/main/java/br/com/senacrs/alp/aulas/Configurar.java
@@ -1,27 +1,92 @@
 package br.com.senacrs.alp.aulas;
 
+import java.io.File;
+import java.util.Hashtable;
+import br.com.senacrs.alp.aulas.InterpretarStrings.TIPO_STRING;
+
 public class Configurar implements ArquivoConfiguracao {
 
-	public Configurar(String arquivoConfiguracao) {
-		// TODO Auto-generated constructor stub
-	}
+	private static final String SUSTENIDO = "#";
+	private static final String IGUAL = "=";
+	private static final String ROT_DIR = "root_dir";
+	private static final String PORT = "port";
+	private static final String ERROR_DIR = "error_dir";
+	private Hashtable<String, String> dicionario;
+	
+	Configurar(String arquivoConfiguracao) {
+		if (arquivoConfiguracao == null) {
+			throw new IllegalArgumentException(
+					"O nome do arquivo não pode ser nulo.");
+		}
 
+		File f = new File(arquivoConfiguracao);
+
+		if (!f.exists()) {
+			throw new IllegalArgumentException(
+					"Arquivo inexistente ou não localizado.");
+		}
+
+		dicionario = new Hashtable<String, String>();
+
+		LeitorArquivos leitorArqivo = new MeuLeitorArq();
+		String[] chaves = null;
+
+		try {
+			chaves = leitorArqivo.lerArquivo(arquivoConfiguracao);
+		} catch (Exception ex) {
+			throw new IllegalArgumentException(
+					"Erro ao tentar ler o arquivo do disco", ex);
+
+		}
+
+		if (chaves == null || chaves.length <= 0)
+			throw new IllegalArgumentException("Arquivo vazio ou inexistente");
+
+		InterpretarStrings validador = new Interpretador();
+
+		for (int i = 0; i < chaves.length; i++) {
+			if (!chaves[i].trim().equals("")) {
+				TIPO_STRING ts = validador.lerString(chaves[i]);
+
+				if (ts == TIPO_STRING.BEM_FORMADA) {
+					String[] valores = chaves[i].split(IGUAL);
+					
+					dicionario.put(valores[0].trim(),
+							LimpaComentarios(valores[1].trim()));
+				} else if (ts == TIPO_STRING.MAL_FORMADA) {
+					throw new IllegalArgumentException(
+							"Existe uma chave invÃ¡lida no arquivo");
+				}
+			}
+		}
+	}
+	private String LimpaComentarios(String valor) {
+		int i = valor.indexOf(SUSTENIDO);
+
+		if(i == 0){
+			throw new IllegalArgumentException("Valor da chave não pode ser vazio");
+		}
+	
+		if (i > -1) {
+			String ret = valor.substring(0, i - 1);
+			return ret;
+		}
+		return valor;
+
+	}
 	@Override
 	public String getRootDir() {
-		// TODO Auto-generated method stub
-		return null;
+		return dicionario.get(ROT_DIR);
 	}
 
 	@Override
 	public String getErrorDir() {
-		// TODO Auto-generated method stub
-		return null;
+		return dicionario.get(ERROR_DIR);
 	}
 
 	@Override
-	public int getPort() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getPort() {		
+		return Integer.parseInt(dicionario.get(PORT));
 	}
 
 }
